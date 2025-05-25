a5d8a19c82617036ee1100ee866487eafdd1b9ae
nevermind <yaoshunyu@whu.edu.cn>
2025-05-25 13:37:04 +0800

    refactor: 调整了部分模块的层级关系

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,61 @@
 pub mod scanner;
 pub mod parser;
+
+
+pub struct TokenUnit {
+    pub token_type: TokenType,
+    pub table_ptr: usize,
+}
+
+
+#[derive(Debug)]
+pub enum TokenType {
+    // 标识符
+    Id,
+
+    // 常量
+    Const,
+
+    // 特殊形式
+    Define,
+    If,
+    List,
+    Cons,
+    Lambda,
+    Display,
+    Quote,
+    QuoteMark,
+
+    // 算术运算符
+    PlusOp,
+    MulOp,
+    MinusOp,
+    DivOp,
+
+    // 逻辑运算符
+    LessThan,
+    GreaterThan,
+    LessEq,
+    GreaterEq,
+    Eq,
+
+    // 界定符（不含双引号）
+    LParen,
+    RParen,
+}
+
+
+#[derive(Debug)]
+pub struct TableItem {
+    pub index: (usize, usize),
+    pub value: Option<ValueType>,
+}
+
+
+#[derive(Debug)]
+pub enum ValueType {
+    Int(isize),
+    Float(f64),
+    Str(String),
+    Bool(bool),
+}
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,5 +1,6 @@
 mod utils;
 use utils::{tokenize, chars2bytes};
+use super::{TokenType, TokenUnit, ValueType, TableItem};
 
 
 pub fn scan(input: &str) -> Result<(Vec<TokenUnit>, Vec<TableItem>), ScanError> {
@@ -52,68 +53,9 @@ fn whitespace_cnt(line: &str) -> usize {
 }
 
 
-pub struct TokenUnit {
-    pub token_type: TokenType,
-    pub table_ptr: usize,
-}
-
-
 pub enum ScanError {
     // 不会出现在Lisp中的字符
     InvalidCharacter((usize, usize)),
     // 不符合词法规则的串
     InvalidToken((usize, usize)),
-}
-
-
-#[derive(Debug)]
-pub enum TokenType {
-    // 标识符
-    Id,
-
-    // 常量
-    Const,
-
-    // 特殊形式
-    Define,
-    If,
-    List,
-    Cons,
-    Lambda,
-    Display,
-    Quote,
-    QuoteMark,
-
-    // 算术运算符
-    PlusOp,
-    MulOp,
-    MinusOp,
-    DivOp,
-
-    // 逻辑运算符
-    LessThan,
-    GreaterThan,
-    LessEq,
-    GreaterEq,
-    Eq,
-
-    // 界定符（不含双引号）
-    LParen,
-    RParen,
-}
-
-
-#[derive(Debug)]
-pub struct TableItem {
-    pub index: (usize, usize),
-    pub value: Option<ValueType>,
-}
-
-
-#[derive(Debug)]
-pub enum ValueType {
-    Int(isize),
-    Float(f64),
-    Str(String),
-    Bool(bool),
 }
